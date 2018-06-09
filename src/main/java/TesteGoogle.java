48131242e3b562bbc0ec51bb73679324fd0e870e
Danilo <daniloso20@gmail.com>
2018-06-09 11:48:17 -0300

    Posição e tamanho do browser

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,8 +10,9 @@ public class TesteGoogle {
 	public void teste() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setPosition(new Point(100, 100));
 		driver.get("https://www.google.com.br/");
 		Assert.assertEquals("Google", driver.getTitle());
-		//teste
+		
 	}
 }
