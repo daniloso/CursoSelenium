5ac50bb696f8b976576719c5c5ce77afbd4507ed
Danilo <daniloso20@gmail.com>
2018-06-09 11:56:07 -0300

    Uso do comando setSize paradefinir tamanho e altura, bem como posição da pagina

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +11,8 @@ public class TesteGoogle {
 	public void teste() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setPosition(new Point(100, 100));
+		//o comando setSize serve para especificar tamanho e altura
+		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("https://www.google.com.br/");
 		Assert.assertEquals("Google", driver.getTitle());
 		
