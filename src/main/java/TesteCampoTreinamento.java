1943d37fe321d7fe69974177af6d17b564ea16d7
Danilo <daniloso20@gmail.com>
2018-06-10 13:43:23 -0300

    Criando uma variavel WebElement e atribuindo o Id buscado a ela, em seguida intanciar uma variavel Select do Selenium e passar a variavel de WebElement criada como parametro para ela. Nesse exemplo esta sendo utilizado uma busca por index.

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -2,7 +2,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
 	@Test
@@ -46,6 +48,20 @@ public class TesteCampoTreinamento {
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
 		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+//		driver.quit();
+		
+	}
+	
+	@Test
+	public void deveInteragirComCombo() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		combo.selectByIndex(3);
+		
 //		driver.quit();
 		
 	}
