8ba53ffc764695dd66efcdbb97e0bd6a359f6c79
Danilo <daniloso20@gmail.com>
2018-06-10 16:55:02 -0300

    Alert com confirmação na caixa de dialogo

--- src/main/java/TestAlert.java
+++ src/main/java/TestAlert.java
@@ -25,4 +25,21 @@ public class TestAlert {
 		driver.quit();
 	}
 	
+	@Test
+	public void deveInteragirComAlertConfirm() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("confirm")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alert.getText());
+		String texto = alert.getText();
+		alert.accept();
+		alert.accept();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+		Thread.sleep(2000);
+		driver.quit();
+	}
 }
