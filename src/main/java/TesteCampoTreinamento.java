eff49501ac02d53f214e89d7660a4a6aba193dde
Danilo <daniloso20@gmail.com>
2018-06-10 14:50:04 -0300

    Selecionado valores multiplos no checkbox

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -93,4 +93,17 @@ public class TesteCampoTreinamento {
 //		driver.quit();
 		
 	}
+	@Test
+	public void deveVerificarValoresComboMultiplo() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("Karate");
+		
+	}
 }
