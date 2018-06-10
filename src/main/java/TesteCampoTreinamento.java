84dd3b23d61a4892ddd3ef454932460146403a30
Danilo <daniloso20@gmail.com>
2018-06-10 11:03:46 -0300

    Criado novo método de teste para interagir com o campo Sugestões

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
-	@Test
+//	@Test
 	public void teste() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -14,7 +14,16 @@ public class TesteCampoTreinamento {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Danilo Oliveira");
 		Assert.assertEquals("Danilo Oliveira", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
-//		driver.getTitle();
+		driver.getTitle();
 		
+	}
+	@Test
+	public void deveInteragirComTextArea() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste\n\fadsfasdfasdasdfasdfasdfasdfa\n\fsdfasdfasdfasd");
+//		Assert.assertEquals("Danilo Oliveira", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 	}
 }
